4eefef340f504651c6eb3d961a178d5af946888b
Najib-Hebrawi <najiphebrawi@hotmail.com>
2022-05-16 16:13:06 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 
+
+
+
+
 function App() {
+
+ const myArray= ["najib","hebrawi"];
+ const myList= myArray.map((names)=><p>{names}</p>)
   return (
     <div className="App">
-      <h1>Hej Najib</h1>
+      <h1>{myList}</h1>
     </div>
   );
 }
